c90cffecae3ad918d8589f375589f6e9e153deee
Tim108 <tsonderen108@gmail.com>
2014-05-06 16:17:13 +0200

    ....

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,9 +1,6 @@
-SELECT p.pid FROM  Person p, Acts a WHERE a.pid = p.pid
-	AND EXISTS (
-		SELECT * FROM Movie m -- the movies exists and a plays in it
-		WHERE m.mid = a.mid
-		AND m.name = 'Back to the Future'
-	)
-);
+SELECT p.pid FROM  Person p, Acts a, Movie m
+WHERE a.pid = p.pid
+	AND m.mid = a.mid
+	AND m.name = 'Back to the Future'
 
--- Shunting rule gebruikt 1x
+-- Shunting rule gebruikt 2x
