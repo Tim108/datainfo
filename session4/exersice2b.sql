c9c52e9eeaa2be2da48c5e2dfa817c054817577a
Tim108 <tsonderen108@gmail.com>
2014-05-27 16:15:22 +0200

    nog meer fix van 2b

--- session4/exersice2b.sql
+++ session4/exersice2b.sql
@@ -3,17 +3,15 @@ SELECT titel FROM Boek WHERE isbn = '0136067018';
 --read1(y):  
 SELECT titel FROM Boek WHERE isbn = '0321228383';
 
---read2(x): 
-SELECT titel FROM Boek WHERE isbn = ' 0136067018';
---write2(x): 
-UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = ' 0136067018';
-
---commit2
-
 --write1(x): 
 UPDATE Boek SET titel = titel || ' deel 1' WHERE isbn = '0136067018';
 --write1(y): 
 UPDATE Boek SET titel = titel || ' deel 1' WHERE isbn = '0321228383';
 
---rollback1
+--read2(x): 
+SELECT titel FROM Boek WHERE isbn = ' 0136067018';
+--write2(x): 
+UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = ' 0136067018';
 
+--rollback1
+--commit2
