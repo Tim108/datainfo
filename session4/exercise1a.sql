15332818000e7382bd163ae4526293eeb4b075bb
Tim108 <tsonderen108@gmail.com>
2014-05-27 14:03:17 +0200

    e1a

---
+++ session4/exercise1a.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Boek(
+isbn BIGINT UNIQUE,
+titel VARCHAR(100),
+auteur VARCHAR(100),
+PRIMARY KEY(isbn)
+);
+
+CREATE TABLE Exemplaar(
+isbn BIGINT,
+volgnummer BIGINT,
+gewicht FLOAT,
+kast BIGINT,
+FOREIGN KEY(isbn) REFERENCES Boek(isbn) ON UPDATE CASCADE ON DELETE CASCADE,
+PRIMARY KEY(isbn,volgnummer)
+);
