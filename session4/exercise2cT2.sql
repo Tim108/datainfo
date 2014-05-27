3055c73df0dbf4765e976966d2bdd1750fa5f6d5
Tim108 <tsonderen108@gmail.com>
2014-05-27 16:23:03 +0200

    2c

---
+++ session4/exercise2cT2.sql
@@ -0,0 +1,8 @@
+﻿
+BEGIN READ UNCOMMITTED
+--read2(x): 
+SELECT titel FROM Boek WHERE isbn = '0136067018';
+
+--write2(x): 
+UPDATE Boek SET titel = titel || ' deel 2' WHERE isbn = '0136067018';
+END
